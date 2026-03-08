064b0fd968dab6a72e7ee407fe9d7ccd93887419
CodeSonnet <18723391974@163.com>
2026-03-08 22:41:51 +0800

    Delete DeepFusion-Crawler directory

--- DeepFusion-Crawler/spiders-files/浏览器.py
+++
@@ -1,4 +0,0 @@
-from DrissionPage import ChromiumOptions
-
-path = r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'  # 请改为你电脑内Chrome可执行文件路径
-ChromiumOptions().set_browser_path(path).save()
