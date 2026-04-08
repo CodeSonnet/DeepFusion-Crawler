fb50ba30f5e344e25d8af64815c098e61dcb28e1
dengbo <18723391974@163.com>
2026-04-08 11:24:47 +0800

    喵喵喵

--- spiders-files/TM_Crawler_Script.py
+++ spiders-files/TM_Crawler_Script.py
@@ -8,24 +8,41 @@ import math
 from DrissionPage import ChromiumPage, ChromiumOptions
 
 # ================= ⚙️ 配置区域 =================
-PRODUCT_NAME = "iQOO 15"  # 产品名称，用于标记数据来源
+PRODUCT_NAME = "真我 GT 7 Pro"  # 产品名称，用于标记数据来源
 
 # 淘宝/天猫商品链接列表
 SHOP_URLS = [
+    #Redmi K80
     #"https://detail.tmall.com/item.htm?abbucket=9&id=855871434996&mi_id=0000CDiYiHh50GPP9_AeDRylgiOvc4gTWYlYOMSfEH0uh1k&ns=1&priceTId=213e05ef17755801666898544e12e7&skuId=5836368211877&spm=a21n57.1.item.3&utparam=%7B%22aplus_abtest%22%3A%2243ad1fbe7ae504d12c2f94396bc6ef97%22%7D&xxc=taobaoSearch",
     #"https://detail.tmall.com/item.htm?ali_refid=a3_430582_1006%3A2558538047%3AH%3A4w26l6jzY%2FxAqjnyc%2BNzWmX9rGaBn3hp%3Af1f42ebff452b3c39e4e0f029cf085a2&ali_trackid=282_f1f42ebff452b3c39e4e0f029cf085a2&id=920957702342&mi_id=0000ZcOw8b3oG9sJqD_BdcjGpE98oRtwhP4EzgLjMv7ixNU&mm_sceneid=1_0_7367393876_0&skuId=6009134897926&spm=a21n57.1.hoverItem.1&utparam=%7B%22aplus_abtest%22%3A%229dbfdd3920a146b5fbf382cf1dd4d836%22%7D&xxc=ad_ztc",
     #"https://detail.tmall.com/item.htm?abbucket=9&fpChannel=101&fpChannelSig=ae94864cfd8ec7ac8763985be57750fedea38d13&id=861599449488&mi_id=0000m_7XSudpXCOKsDL3IgLpxQzRyacxTENV-lsaiy_50s8&ns=1&skuId=5847817715225&spm=a21n57.1.hoverItem.2&u_channel=bybtqdyh&umpChannel=bybtqdyh&utparam=%7B%22aplus_abtest%22%3A%22dd1df772fc451a51b0f48ee46c829d84%22%7D&xxc=taobaoSearch",
     #"https://detail.tmall.com/item.htm?abbucket=9&id=856717087068&mi_id=0000LM97zWZiKl905lXoKmzvlPRbeGQ4IfWXJckiwG6i3F8&ns=1&priceTId=213e037117755802164237741e0f7b&skuId=6014349265039&spm=a21n57.1.hoverItem.4&utparam=%7B%22aplus_abtest%22%3A%22cd1a28334aa7829b42623511c9a021ce%22%7D&xxc=taobaoSearch",
-    #"",
+    # iQOO 15
     #"https://detail.tmall.com/item.htm?ali_refid=a3_430582_1006%3A1104380429%3AH%3ArljAQrgSFsZqU%2FRMoIX%2B%2BQ%3D%3D%3Acf2f12723898ba63fea986290afa54b9&ali_trackid=318_cf2f12723898ba63fea986290afa54b9&fpChannel=101&fpChannelSig=c7e3704e9f6664f6e2c914226fa5c8b99875a513&id=972194079657&mi_id=0000re6YTknGuUNMeVex8BmbK2GkuB9YHDzC3ldStfaOmCw&mm_sceneid=0_0_31676741_0&skuId=6046680492075&spm=a21n57.1.hoverItem.1&utparam=%7B%22aplus_abtest%22%3A%229ecc7a9d60821ff02e20105f34bd38d0%22%7D&xxc=ad_ztc",
     #"https://detail.tmall.com/item.htm?ali_refid=a3_420434_1006%3A1355700140%3AH%3AR25HnSmiPvTGiwM6UkVNJb2Ozm4nW%2FMy%3Ae6d6d82fa593975e5c9263269a79d276&ali_trackid=318_e6d6d82fa593975e5c9263269a79d276&id=974827989996&mi_id=0000Y4VYURRVJd51jGgQERc_zrNAEUoMwlRm-JC6zFUPJkI&mm_sceneid=0_0_1426190081_0&skuId=6211567786733&spm=a21n57.1.hoverItem.2&utparam=%7B%22aplus_abtest%22%3A%22bd753e3a177a57a87f40082d8bb3f2b8%22%7D&xxc=ad_ztc",
     #"https://detail.tmall.com/item.htm?ali_refid=a3_420434_1006%3A1355700140%3AH%3ASgH2dPKbSzf%2FoABshKJ2sWTWcdKaiVbd%3A493abffe9cbba04fa0564948612b0bd4&ali_trackid=318_493abffe9cbba04fa0564948612b0bd4&id=973611503138&mi_id=0000ype4IEO_HpSmxXow-AY8vbtkIDWUOfZdlWxmvLucVDw&mm_sceneid=0_0_1426190081_0&skuId=6212053255178&spm=a21n57.1.hoverItem.4&utparam=%7B%22aplus_abtest%22%3A%2204d8d24ed6d13637dd20686940cd7183%22%7D&xxc=ad_ztc",
-    "https://detail.tmall.com/item.htm?abbucket=9&fpChannel=101&fpChannelSig=7c7b1b0c29d82644c0540fd5710442d91e6299a3&id=972708634672&mi_id=0000lUxFCOYD50a_cc6qOm2upIUwDsBTSicXuoFUgYq61FM&ns=1&skuId=6212469071093&spm=a21n57.1.hoverItem.8&u_channel=bybtqdyh&umpChannel=bybtqdyh&utparam=%7B%22aplus_abtest%22%3A%2203d177d7bd30ac83ee6ff148dbd7c7fb%22%7D&xxc=taobaoSearch",
+    #没爬完"https://detail.tmall.com/item.htm?abbucket=9&fpChannel=101&fpChannelSig=7c7b1b0c29d82644c0540fd5710442d91e6299a3&id=972708634672&mi_id=0000lUxFCOYD50a_cc6qOm2upIUwDsBTSicXuoFUgYq61FM&ns=1&skuId=6212469071093&spm=a21n57.1.hoverItem.8&u_channel=bybtqdyh&umpChannel=bybtqdyh&utparam=%7B%22aplus_abtest%22%3A%2203d177d7bd30ac83ee6ff148dbd7c7fb%22%7D&xxc=taobaoSearch",
+    #荣耀 Magic 7 Pro,
+    #"https://detail.tmall.com/item.htm?abbucket=9&fpChannel=101&fpChannelSig=78588ca181262d9744ff98867f2ce2527a927090&id=857554269384&mi_id=0000mXVhX1G1eNPllsArFZjvRhgN3SzarWnWbaDNAXudFUE&ns=1&skuId=5665143268886&spm=a21n57.1.hoverItem.1&u_channel=bybtqdyh&umpChannel=bybtqdyh&utparam=%7B%22aplus_abtest%22%3A%22f218c8ae0d133ec100713f0b6e363ac2%22%7D&xxc=taobaoSearch",
+    #"https://item.taobao.com/item.htm?abbucket=9&id=993510523300&mi_id=0000wVRb7FQrBrQn3Ls2YIDEjkSEY5QVrb-e6II0xgoICDo&ns=1&priceTId=213e075317756157898294555e1129&skuId=5973116117685&spm=a21n57.1.hoverItem.4&utparam=%7B%22aplus_abtest%22%3A%2264b26e1aa0258994fc88275c51dea065%22%7D&xxc=taobaoSearch",
+    #"https://item.taobao.com/item.htm?abbucket=9&id=857433026724&mi_id=00009V6hCAl9bmIGp5_C9NctpE86OBxP7BDjZ5OfN2W1yng&ns=1&priceTId=213e075317756157898294555e1129&skuId=5835387475627&spm=a21n57.1.hoverItem.5&utparam=%7B%22aplus_abtest%22%3A%2273eab142b26ba7421b59f4375944ce2a%22%7D&xxc=taobaoSearch",
+    #"https://item.taobao.com/item.htm?abbucket=9&id=862719166593&mi_id=0000PM21zZDvrg4cvsqJKwicipJZGP3qhrgGc2XNvl_XL0s&ns=1&priceTId=213e075317756157898294555e1129&skuId=5685343457585&spm=a21n57.1.hoverItem.7&utparam=%7B%22aplus_abtest%22%3A%22ed98126a0a5adc70699edb73e76da433%22%7D&xxc=taobaoSearch",
+    #"https://item.taobao.com/item.htm?abbucket=9&id=990345546327&mi_id=0000U4MUIypAYimtfBTq7EsvVUfbB0FdEUBBiLbQwI4-E4Q&ns=1&priceTId=213e075317756157898294555e1129&skuId=6125191890327&spm=a21n57.1.hoverItem.8&utparam=%7B%22aplus_abtest%22%3A%228cb520bbf8c6d22b6d8296e878b6698f%22%7D&xxc=taobaoSearch",
+    #"https://item.taobao.com/item.htm?abbucket=9&id=855481945158&mi_id=00001lxeBA9SRsphIW1pqFehccBOJ_v4IPQOCIXEo3aQ7f4&ns=1&priceTId=213e075317756157898294555e1129&skuId=6156300510497&spm=a21n57.1.hoverItem.10&utparam=%7B%22aplus_abtest%22%3A%220729bdc6f1b6a0c8a5841700db46b7cc%22%7D&xxc=taobaoSearch",
+    #"https://detail.tmall.com/item.htm?abbucket=9&id=838582843115&mi_id=0000UofW0fbyKgB02NbB12f_1QNQ-Z9K99VRVJaLfQ0vaE0&ns=1&priceTId=213e075317756157898294555e1129&skuId=5879787064632&spm=a21n57.1.hoverItem.11&utparam=%7B%22aplus_abtest%22%3A%22765d1b27efa7d617aacfc09827c4b6c4%22%7D&xxc=taobaoSearch",
+    #真我 GT 7 Pro
+    "https://detail.tmall.com/item.htm?abbucket=9&id=838577791595&mi_id=0000K6JycvsvjYwhyB3zkH9CazEPrW68sOc0TW9W9fVTxAY&ns=1&priceTId=213e076717756167845427286e1112&skuId=5639845648822&spm=a21n57.1.hoverItem.2&utparam=%7B%22aplus_abtest%22%3A%22840d76d9b22855cb51ff554e5dfa2ddb%22%7D&xxc=taobaoSearch",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
     "",
 ] #URL
 
 SCROLL_TIMES = 1000  # 淘宝有效评论通常较少，1000轮绝对够用了
-JSON_FILE = 'data/TaoBao/taobao_iQOO_15.json'
+JSON_FILE = 'data/TaoBao/taobao_真我_GT7_Pro.json'
 USER_DATA_DIR = './Taobao_User_Data'  # 🔥 淘宝专属：用于保存你的登录状态(Cookies)
 # ===============================================
 
